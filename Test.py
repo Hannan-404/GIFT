e8007da2b18b2562f16b4b3d782758a2e9a34c2a
HANNAN ANSARI <90706836+Hannan-404@users.noreply.github.com>
2023-01-22 15:24:22 +0500

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,3 +1,11 @@
 import os
 os.system('xdg-open https://facebook.com/groups/3465474777107830/')
-import TestV1
+#Ase Kia Dekh raha Bhai
+#32bit/64bit run krne ka new method he
+try:
+ import TestV1
+except:
+ try:
+  import TestV2
+ except:
+  print('Device Not Support')
